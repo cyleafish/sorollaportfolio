48ef3e73bb2a1e66969ad0de092fcc14c7b28ca5
leafish <i@leafish.xyz>
2025-03-31 13:58:52 +0800

    更改錯誤

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,53 +3,23 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
-}
+	const collections = ['blog', 'projects', 'working_experience', 'activity_experience'];
+	let items = [];
 
-export async function GET(context) {
-	const projects = await getCollection('projects');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: projects.map((projects) => ({
-			...projects.data,
-			link: `/projects/${projects.id}/`,
-		})),
-	});
-}
+	for (const collection of collections) {
+		const data = await getCollection(collection);
+		items = items.concat(
+			data.map((item) => ({
+				...item.data,
+				link: `/${collection}/${item.id}/`,
+			}))
+		);
+	}
 
-export async function GET(context) {
-	const working_experience = await getCollection('working_experience');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: working_experience.map((working_experience) => ({
-			...working_experience.data,
-			link: `/working_experience/${working_experience.id}/`,
-		})),
-	});
-}
-
-export async function GET(context) {
-	const activity_experience = await getCollection('activity_experience');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: activity_experience.map((activity_experience) => ({
-			...activity_experience.data,
-			link: `/activity_experience/${activity_experience.id}/`,
-		})),
+		items,
 	});
 }
